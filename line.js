fc5b6af73f2fa32c219261218c73e475bf37f07e
msewak <sewak.madhavi@gmail.com>
2010-10-26 21:01:22 -0700

    the line function now prints all the lines in the forest variable

--- line.js
+++ line.js
@@ -2,8 +2,7 @@ function line() {
 //	var data = pv.range(1990, 2010, .1).map(function(x) {
 //    return {x: x, y: Math.sin(x) + Math.random() * .5 + 2};
 //  });
-  var data = forest['AFG'];
-		
+  
 	var w = 860,
     h = 180,
     x = pv.Scale.linear(startYear, endYear).range(0, w),
@@ -18,31 +17,62 @@ var vis = new pv.Panel()
     .right(10)
     .top(5);
 
+var i
+var dummy = []
+for (i in forest) {
+	dummy.push(forest[i]) 
+}
+
+vis.add(pv.Panel)
+	.data(function(){
+		return dummy;
+	})
+  .add(pv.Line)
+	.data(function(d) {
+		console.log("message");
+		console.log(d);
+		return d;
+		})
+		.left(function(d) {
+		console.log(d);
+		return x(d.year);
+		})
+	.bottom(function(d) {
+		return y(d.value);
+		})
+    .lineWidth(2);
+		
+//  .add(pv.Label)
+//    .visible(function() this.index == S-1)
+//    .textBaseline("middle")
+//    .textMargin(6)
+//    .text(ft);
+	
 /* Y-axis and ticks. */
-vis.add(pv.Rule)
-    .data(y.ticks(5))
-    .bottom(y)
-    .strokeStyle(function(d) { return d ? "#eee" : "#000";})
-  .anchor("left").add(pv.Label)
-    .text(y.tickFormat);
-
-/* X-axis and ticks. */
-vis.add(pv.Rule)
-    .data(x.ticks())
-    .visible(function(d) { return d;})
-    .left(x)
-    .bottom(-5)
-    .height(5)
-  .anchor("bottom").add(pv.Label)
-    .text(x.tickFormat);
-
-vis.add(pv.Line)
-    .data(data)
-    .left(function(d) { return x(d.year); 
-	})
-    .bottom(function(d) { return y(d.value);
-	})
-    .lineWidth(3);
+//vis.add(pv.Rule)
+//    .data(y.ticks(5))
+//    .bottom(y)
+//    .strokeStyle(function(d) { return d ? "#eee" : "#000";})
+//  .anchor("left").add(pv.Label)
+//    .text(y.tickFormat);
+//
+///* X-axis and ticks. */
+//vis.add(pv.Rule)
+//    .data(x.ticks())
+//    .visible(function(d) { return d;})
+//    .left(x)
+//    .bottom(-5)
+//    .height(5)
+//  .anchor("bottom").add(pv.Label)
+//    .text(x.tickFormat);
+//
+//vis.add(pv.Line)
+//    .data(data)
+//    .left(function(d) { return x(d.year); 
+//	})
+//    .bottom(function(d) { return y(d.value);
+//	})
+//    .lineWidth(3);
 
 vis.render();
 }
