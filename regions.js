ececd52bf6fbae39f629727320e8e64615c8021a
Juan Manuel Tamayo <jtamayo@cs.stanford.edu>
2010-10-28 21:54:57 -0700

    added some sort of color schema

---
+++ regions.js
@@ -0,0 +1,14 @@
+var regions = {
+	1: "Central Africa",
+	2: "Southeastern Africa",
+	3: "Northern Africa",
+	4: "Southern Asia",
+	5: "Asia Pacific",
+	6: "Middle East",
+	7: "Eastern Asia",
+	8: "South America",
+	9: "Eastern Europe",
+	10: "North America",
+	11: "Western Europe",
+	12: "Japan"
+}
